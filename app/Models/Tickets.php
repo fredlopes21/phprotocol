85ba87e3274be23620e2583d086cfacbd0276c75
lucas <lucasrochabr@outlook.com>
2019-08-22 15:25:18 -0300

    adds ticket model

---
+++ app/Models/Tickets.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Ticket
+ *
+ * @author    Lucas Rocha <lucasrochabr[at]outlook[dot]com>
+ * @copyright    Copyleft (c) Lucas Rocha
+ */
+class Ticket extends Model
+{
+	use SoftDeletes;
+
+	protected $table = 'tickets';
+
+	protected $fillable = [
+		'user',
+		'company',
+		'channel',
+		'type',
+		'module',
+		'description',
+	];
+}
