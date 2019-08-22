c6dec7578ea3d732382fa1a966f1d98cbf4e358c
lucas <lucasrochabr@outlook.com>
2019-08-22 13:42:14 -0300

    não reinventarás a roda

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # phprotocol
-Gestão de chamados
+Gestão de chamados baseado no [Slim-Born do HavenShen](https://github.com/HavenShen/slim-born)
+
+
+## Installing
+Import slimborn_2016-04-19.sql into your Database, creates you .env file based on .env.example. Run:
+```sh
+composer install
+```
 
 
 ## Running
 ```sh
 php -S 0.0.0.0:8888 -t public public/index.php
+```
+
+
+## Testing
+```sh
+vendor/bin/phpunit
 ```
--- public/index.php
+++ public/index.php
@@ -1,50 +1,5 @@
 <?php
-use MyApp\Handlers\ErrorHandler;
-use MyApp\Handlers\ShutdownHandler;
-use Slim\Exception\HttpInternalServerErrorException;
-use Slim\Factory\AppFactory;
-use Slim\Factory\ServerRequestCreatorFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$displayErrorDetails = true;
-
-$app = AppFactory::create();
-$callableResolver = $app->getCallableResolver();
-$responseFactory = $app->getResponseFactory();
-
-$serverRequestCreator = ServerRequestCreatorFactory::create();
-$request = $serverRequestCreator->createFromGlobals();
-
-$errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
-$shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
-register_shutdown_function($shutdownHandler);
-
-// Add Routing Middleware
-$app->addRoutingMiddleware();
-
-// Add Error Handling Middleware
-$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, false, false);
-$errorMiddleware->setDefaultErrorHandler($errorHandler);
-
-// Add Eloquent
-// Database information
-$settings = array(
-    'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'database' => '',
-    'username' => '',
-    'password' => '',
-    'collation' => 'utf8_general_ci',
-    'prefix' => ''
-);
-
-// Bootstrap Eloquent ORM
-$connFactory = new \Illuminate\Database\Connectors\ConnectionFactory();
-$conn = $connFactory->make($settings);
-$resolver = new \Illuminate\Database\ConnectionResolver();
-$resolver->addConnection('default', $conn);
-$resolver->setDefaultConnection('default');
-\Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver);
+require __DIR__ . '/../bootstrap/app.php';
 
 $app->run();
